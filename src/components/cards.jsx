e2a9a54271fdb01ab541efe46e8e39c85246825c
AbdullahAssi <pakabdullah225@gmail.com>
2023-06-30 10:24:54 -0700

    Card animated

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,31 +1,52 @@
 import React, { useState } from "react";
 import  {card}  from "../constants";
 import  Collection_cards  from "./collection_cards";
+import { motion } from "framer-motion";
+import { useInView } from 'react-intersection-observer';
 
-function Card (){
+function Card() {
+    const [visible, setVisible] = useState(4);
+    const [ref, inView] = useInView({
+        triggerOnce: true, 
+        threshold: 0.2, 
+    });
 
-    const [visible , setVisible] = useState(4);
+    const showMoreItems = () => {
+        setVisible(prevState => prevState + 4);
+    };
 
-    const showMoreItems =() =>{
-        setVisible(prevState => prevState +4 );
-    }
     return (
         <div className="p-10 sm:p-20 cards flex flex-wrap items-center justify-center gap-12 box-shadow: 0 5px 13px hsla(214, 19%, 29%, 0.2); max-w-[1280px]">
-            {card.slice(0,visible).map(cards => (
-                <Collection_cards 
-                    key = {cards.id}
-                    nft = {cards.nft}
-                    seller = {cards.seller}
-                    sellerprofile = {cards.sellerProfile}
-                    des = {cards.description}
-                    price = {cards.price}
-                    highestbid = {cards.HighestBid}
-                    eth = {cards.ethereum}
-                    bolt = {cards.flash}
-                />
+            {card.slice(0, visible).map((cards, i) => (
+                <div
+                    key={cards.id}
+                    ref={i === 0 ? ref : null} 
+                >
+                    {inView && ( 
+                        <motion.div
+                            initial={{ opacity: 0, translateX: -50, translateY: 0 }}
+                            animate={{ opacity: 1, translateX: 0, translateY: 0 }}
+                            transition={{ duration: 0.3, delay: i * 0.2 }}
+                        >
+                            <Collection_cards
+                                nft={cards.nft}
+                                seller={cards.seller}
+                                sellerprofile={cards.sellerProfile}
+                                des={cards.description}
+                                price={cards.price}
+                                highestbid={cards.HighestBid}
+                                eth={cards.ethereum}
+                                bolt={cards.flash}
+                            />
+                        </motion.div>
+                    )}
+                </div>
             ))}
-            <button onClick={showMoreItems} className="text-white btn">Explore More</button>
-        </div>
-    )
-}
+
+                <button onClick={showMoreItems} className="text-white btn">
+                    Explore More
+                </button>
+            </div>
+        );
+    }
 export default Card
--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Contact() {
     return (
-        <div className='flex flex-col justify-center items-center h-[50vh] xl:h-[50vh] p-8 sm:p-0 mb-[-20px] xl:mb-[-100px]'>
+        <div className='flex flex-col justify-center items-center h-[50vh] xl:h-[50vh] p-8 sm:p-0 mb-[-20px] xl:mb-[-100px] mt-20 sm:mt-0'>
             <p className='text-5xl font-semibold text-white text-center py-4'>Get In Touch</p>
             <p className='text-[15px] max-w-[500px] text-center pb-6 sm:text-3xl'>We are a huge marketplace dedicated to connecting great artists of all Metalink with their fans and unique token collectors!</p>
             <button className='p-4 bg-[#4d1fad] rounded-lg text-white text-[17px]'>Contact Now</button>
